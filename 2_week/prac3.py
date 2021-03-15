2f5d07395f0264d14a337a6b3f1776b7891b353a
noh-yj <goky456@gmail.com>
2021-03-15 18:22:18 +0900

    저녁먹기 전 학습업로드

--- 2_week/prac3.py
+++ 2_week/prac3.py
@@ -51,3 +51,153 @@
 # print(sum(b))
 
 # 스택
+
+# import sys
+
+# n = int(sys.stdin.readline())
+# stack = []
+
+# for i in range(n):
+#     order = sys.stdin.readline().split()
+#     if order[0] == 'push':
+#         stack.append(int(order[1]))
+#     elif order[0] == 'pop':
+#         if len(stack) == 0:
+#             print(-1)
+#         else:
+#             a = stack.pop()
+#             print(a)
+#     elif order[0] == 'size':
+#         print(len(stack))
+#     elif order[0] == 'empty':
+#         if len(stack) == 0:
+#             print(1)
+#         else:
+#             print(0)
+#     elif order[0] == 'top':
+#         if len(stack) == 0:
+#             print(-1)
+#         else:
+#             print(stack[-1])
+
+# 제로
+
+# k = int(input())
+# stack = []
+# for i in range(k):
+#     num = int(input())
+#     stack.append(num)
+#     if num == 0:
+#         stack.pop()
+#         stack.pop()
+
+# print(sum(stack))
+
+# 괄호
+
+# t = int(input())
+
+# for i in range(t):
+#     stack = []
+#     chk = True
+#     vps = input()
+#     for i in vps:
+#         if i == '(':
+#             stack.append(i)
+#         elif i == ')':
+#             if len(stack) == 0:
+#                 chk = False
+#             elif stack[-1] == '(':
+#                 stack.pop()
+#             else:
+#                 chk = False
+#     if chk and len(stack) == 0:
+#         print('YES')
+#     else:
+#         print('NO')
+
+
+# 큐 2
+# from collections import deque
+# import sys
+# n = int(sys.stdin.readline())
+# queue = deque()
+# for i in range(n):
+#     order = sys.stdin.readline().split()
+
+#     if order[0] == 'push':
+#         queue.append(int(order[1]))
+#     elif order[0] == 'pop':
+#         if len(queue) == 0:
+#             print(-1)
+#         else:
+#             a = queue.popleft()
+#             print(a)
+#     elif order[0] == 'size':
+#         print(len(queue))
+#     elif order[0] == 'empty':
+#         if len(queue) == 0:
+#             print(1)
+#         else:
+#             print(0)
+#     elif order[0] == 'front':
+#         if len(queue) == 0:
+#             print(-1)
+#         else:
+#             print(queue[0])
+#     elif order[0] == 'back':
+#         if len(queue) == 0:
+#             print(-1)
+#         else:
+#             print(queue[-1])
+
+# BFS와 DFS
+
+# from collections import deque
+# graph = {}
+# n, m, v = map(int, input().split())
+
+# for i in range(n):
+#     graph[i+1] = []
+# for i in range(m):
+#     x, y = map(int, input().split())
+#     graph[x] += [y]
+#     graph[y] += [x]
+
+# start_node = v
+
+
+# def dfs_stack(graph, start_node):
+#     stack = [start_node]
+#     visited = []
+#     while stack:
+#         log = stack.pop()
+#         if log not in visited:
+#             print(log, end=' ')
+#         graph[log].sort(reverse=True)
+#         visited.append(log)
+#         for i in graph[log]:
+#             if i not in visited:
+#                 stack.append(i)
+
+
+# def bfs_queue(graph, start_node):
+#     queue = deque()
+#     queue.append(start_node)
+#     visited = []
+#     while queue:
+#         log = queue.popleft()
+#         if log not in visited:
+#             print(log, end=' ')
+#         graph[log].sort()
+#         visited.append(log)
+#         for i in graph[log]:
+#             if i not in visited:
+#                 queue.append(i)
+
+
+# dfs_stack(graph, start_node)
+# print()
+# bfs_queue(graph, start_node)
+
+# 2108 통계학
