d1ac28ba8c277664e9784b470aa23a8ebca24a04
noh-yj <goky456@gmail.com>
2021-06-13 17:07:50 +0900

    upload

--- Implementation/prac.py
+++ Implementation/prac.py
@@ -56,58 +56,58 @@
 # print(cnt)
 
 # 게임 개발
-n, m = map(int, input().split())
-# m*n 공간 생성
-d = [[0]*m for _ in range(n)]
-# direction = 0: 북 1: 동 2: 남 3: 서
-x, y, direction = map(int, input().split())
+# n, m = map(int, input().split())
+# # m*n 공간 생성
+# d = [[0]*m for _ in range(n)]
+# # direction = 0: 북 1: 동 2: 남 3: 서
+# x, y, direction = map(int, input().split())
 
-d[x][y] = 1  # 시작 좌표
-# 현재 지형 정보 추가
-arr = []
-for i in range(n):
-    arr.append(list(map(int, input().split())))
+# d[x][y] = 1  # 시작 좌표
+# # 현재 지형 정보 추가
+# arr = []
+# for i in range(n):
+#     arr.append(list(map(int, input().split())))
 
-# 방향 정의
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+# # 방향 정의
+# dx = [-1, 0, 1, 0]
+# dy = [0, 1, 0, -1]
 
-# 왼쪽으로 회전
-
-
-def turn_left():
-    global direction
-    direction -= 1
-    if direction == -1:
-        direction = 3
+# # 왼쪽으로 회전
 
 
-count = 1
-turn_time = 0
-while True:
-    turn_left()
-    nx = x + dx[direction]
-    ny = y + dy[direction]
+# def turn_left():
+#     global direction
+#     direction -= 1
+#     if direction == -1:
+#         direction = 3
 
-    if d[nx][ny] == 0 and arr[nx][ny] == 0:
-        d[nx][ny] = 1
-        x = nx
-        y = ny
-        count += 1
-        turn_time = 0
-        continue
-    else:
-        turn_time += 1
 
-    if turn_time == 4:
-        nx = x - dx[direction]
-        ny = y - dy[direction]
+# count = 1
+# turn_time = 0
+# while True:
+#     turn_left()
+#     nx = x + dx[direction]
+#     ny = y + dy[direction]
 
-        if arr[nx][ny] == 0:
-            x = nx
-            y = ny
-        else:
-            break
-        turn_time = 0
+#     if d[nx][ny] == 0 and arr[nx][ny] == 0:
+#         d[nx][ny] = 1
+#         x = nx
+#         y = ny
+#         count += 1
+#         turn_time = 0
+#         continue
+#     else:
+#         turn_time += 1
 
-print(count)
+#     if turn_time == 4:
+#         nx = x - dx[direction]
+#         ny = y - dy[direction]
+
+#         if arr[nx][ny] == 0:
+#             x = nx
+#             y = ny
+#         else:
+#             break
+#         turn_time = 0
+
+# print(count)
